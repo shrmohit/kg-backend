1a35f45b06a0b59451f9c7275e63f116842f4756
mohit sharma <mohitsharma7258@gmail.com>
2025-04-01 11:26:31 +0530

    it show content in web

--- Lecture-4/app.js
+++ Lecture-4/app.js
@@ -2,6 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("url", req.url, "method", req.method, "headers", req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 const PORT = 3000;
