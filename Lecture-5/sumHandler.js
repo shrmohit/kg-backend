ddd52a08b7efaaad3361faab876638ca6010ca90
mohit sharma <mohitsharma7258@gmail.com>
2025-04-02 00:54:55 +0530

    calculator

---
+++ Lecture-5/sumHandler.js
@@ -0,0 +1,34 @@
+const sumHandler = (req, res) => {
+  console.log("In sum Request Handler", req.url);
+
+  const body = [];
+  req.on("data", (chunks) => {
+    console.log(chunks);
+    body.push(chunks);
+  });
+
+  req.on("end", () => {
+    const parsedBody = Buffer.concat(body).toString();
+    console.log(parsedBody);
+    const params = new URLSearchParams(parsedBody);
+    const bodyObj = Object.fromEntries(params);
+    console.log(params);
+    const result = Number(bodyObj.first) + Number(bodyObj.second);
+    console.log(result);
+
+    res.write(`
+      <html>
+    <head>
+      <title>Result</title>
+    </head>
+    <body>
+      <h1>Your Sum is ${result}</h1>
+   
+    </body>
+  </html>
+    `);
+    return res.end();
+  });
+};
+
+exports.sumHandler = sumHandler;
