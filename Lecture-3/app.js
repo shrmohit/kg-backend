78caaf0f23f4b9590497692f99979fe585928c6b
mohit sharma <mohitsharma7258@gmail.com>
2025-04-01 10:33:14 +0530

    create first node server

--- Lecture-3/app.js
+++ Lecture-3/app.js
@@ -4,4 +4,7 @@ const server = http.createServer((req, res) => {
   console.log(req);
 });
 
-server.listen(3000);
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is Running at http://localhost:${PORT}`);
+});
