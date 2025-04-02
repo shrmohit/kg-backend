d0256bdcb3e48eb3f8c8c6e9b5cb09cd94a37b55
mohit sharma <mohitsharma7258@gmail.com>
2025-04-02 18:53:16 +0530

    add middleware

--- Chapter-9/app.js
+++ Chapter-9/app.js
@@ -6,8 +6,17 @@ const express = require("express");
 const requestHandler = require("./user");
 
 const app = express();
-// express is used to create application and export its, app also take req and res as parameters
+//1. express is used to create application and export its, app also take req and res as parameters
+//2.use is used to create middleware
+app.use((req, res, next) => {
+  console.log("Came in first middleware", req.url, req.method);
+  next();
+  //next is used to call next middleware
+});
 
+app.use((req, res, next) => {
+  console.log("Came in Second middleware", req.url, req.method);
+});
 const server = http.createServer(app);
 
 const PORT = 3002;
