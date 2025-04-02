ec3965d8bc7e60616f63e59545e7b567acdf7e5f
mohit sharma <mohitsharma7258@gmail.com>
2025-04-02 22:15:47 +0530

    path in use

--- Chapter-9/app.js
+++ Chapter-9/app.js
@@ -1,25 +1,33 @@
-// core Module
-const http = require("http");
 // External Module
 const express = require("express");
-// Local Module
+
 const requestHandler = require("./user");
 
 const app = express();
 //1. express is used to create application and export its, app also take req and res as parameters
 //2.use is used to create middleware
-app.use((req, res, next) => {
+//3. favicon means small icon in the browser tab
+//4. app is used for create sever and also work as createserver
+// 5. send means end
+// add middleware
+app.use("/", (req, res, next) => {
   console.log("Came in first middleware", req.url, req.method);
   next();
+  res.send("<h1>First Middleware</h1>");
   //next is used to call next middleware
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
+  console.log("Came in another middleware", req.url, req.method);
+  res.send("<h1>Another Middleware</h1>");
+  //next is used to call next middleware
+});
+
+app.use("/submit-details", (req, res, next) => {
   console.log("Came in Second middleware", req.url, req.method);
 });
-const server = http.createServer(app);
 
 const PORT = 3002;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
